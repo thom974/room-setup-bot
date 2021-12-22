8c1c17ef106ba27f7766c5a1d2488964963cf4db
thom974 <thomas.q.luc@gmail.com>
2021-12-22 15:02:45 -0500

    refactored to use await

--- commands/start.js
+++ commands/start.js
@@ -16,53 +16,47 @@ module.exports = {
         const balance = 0   // $0 dollars
         const jobID = 1     // Starting job "Cashier"
 
-        dbClient.query(`SELECT discord_id FROM users WHERE discord_id=${userID}`, (err, res) => {
-            if (err) throw err
-            
-            // Check if user already in database
-            if (!res.rows.length) {
-                // Add record to 'users' table 
-                dbClient.query(`INSERT INTO users (discord_id, balance, job_id, join_date) 
-                    VALUES ('${userID}', '${balance}', '${jobID}', NOW())`, (err, res) => {
-                        if (err) throw err
+        const { rows: q1 }  = await dbClient.query(`SELECT discord_id FROM users WHERE discord_id=${userID}`)
+        
+        if (!q1.length) {
+            // Add record to 'users' table 
+            const { rows: q2 } = await dbClient.query(`INSERT INTO users (discord_id, balance, job_id, join_date) 
+                VALUES ('${userID}', '${balance}', '${jobID}', NOW())`)
+                
+            interaction.reply({
+                content: `Welcome aboard, ${username}!` 
+            })
 
-                        interaction.reply({
-                            content: `Welcome aboard, ${username}!` 
-                        })
-                    })
+            // Assign three random tasks 
+            const { rows: q3 } = await dbClient.query(`SELECT task_id FROM tasks`)
 
-                // Assign three random tasks 
-                dbClient.query(`SELECT task_id FROM tasks`, (err, res) => {
-                    if (err) throw err
+            const availableTasks = q3.map(task => task.task_id)
+                
+            // Fetch three random tasks with no overlap, (make cleaner?)
+            const choices = []
+            for (let i=0; i<3; i++) {
+                let n = -1
 
-                    const availableTasks = res.rows.map(task => task.task_id)
-                    
-                    // Fetch three random tasks with no overlap, (make cleaner?)
-                    const choices = []
-                    for (let i=0; i<3; i++) {
-                        let n = -1
+                while (n < 0 || choices.includes(n)) {
+                    n = Math.floor(Math.random() * (availableTasks.length - 1)) + 1
+                }
 
-                        while (n < 0 || choices.includes(n)) {
-                            n = Math.floor(Math.random() * (availableTasks.length - 1)) + 1
-                        }
-
-                        choices.push(n)
-                    }
-
-                    // Add tasks to user_tasks table 
-                    
-                    dbClient.query(`INSERT INTO users_tasks (discord_id, task_one_id, task_two_id, task_three_id)
-                        VALUES ('${userID}', '${choices[0]}', '${choices[1]}','${choices[2]}')`, (err, res) => {
-                            if (err) throw err
-                    })
-                })
-
-            } else {
-                interaction.reply({
-                    content: `You've already registered your account, ${username}!`,
-                    ephemeral: true 
-                })
+                choices.push(n)
             }
-        })
+
+            // Add tasks to user_tasks table 
+            const { rows: q4 } = await dbClient.query(`INSERT INTO users_tasks (discord_id, task_one_id, task_two_id, task_three_id)
+                VALUES ('${userID}', '${choices[0]}', '${choices[1]}','${choices[2]}')`)
+            
+            // Add default job to users_jobs table 
+            const { rows: q5 } = await dbClient.query(`INSERT INTO users_jobs (discord_id, job_id) VALUES
+            ('${userID}', '${jobID}')`)
+            
+        } else {
+            interaction.reply({
+                content: `You've already registered your account, ${username}!`,
+                ephemeral: true 
+            })
+        }
     }
 }
