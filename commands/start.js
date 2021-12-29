982a8312cd57d9ee36d7769a73b1f79b0caffbf9
thom974 <74675902+thom974@users.noreply.github.com>
2021-12-29 14:05:27 -0500

    added storage initialization

--- commands/start.js
+++ commands/start.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { jobMarketUpdate } = require('./helpers/jobMarketUpdate')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -22,13 +24,9 @@ module.exports = {
         
         if (!q1.length) {
             // Add record to 'users' table 
-            const { rows: q2 } = await dbClient.query(`INSERT INTO users (discord_id, balance, job_id, join_date, job_last_pay, job_paycheck, job_join_date) 
-                VALUES ('${userID}', '${balance}', '${jobID}', NOW(), NOW(), '${paycheck}', NOW())`)
+            const { rows: q2 } = await dbClient.query(`INSERT INTO users (discord_id, balance, job_id, join_date, job_last_pay, job_paycheck, job_join_date, room_needs_update) 
+                VALUES ('${userID}', '${balance}', '${jobID}', NOW(), NOW(), '${paycheck}', NOW(), 'true')`)
                 
-            await interaction.reply({
-                content: `Welcome aboard, ${username}!` 
-            })
-
             // Assign three random tasks 
             const { rows: q3 } = await dbClient.query(`SELECT task_id FROM tasks`)
 
@@ -56,6 +54,26 @@ module.exports = {
             
             // Add jobs to job market
             jobMarketUpdate(interaction)
+
+            // Add room code to disk storage
+            const storageDir = process.env.STORAGEDIR
+            const userStorageDir = path.join(storageDir, `${userID}`)
+
+            fs.mkdir(userStorageDir, (err) => {     // Make new folder for user in disk storage
+                if (err) throw err
+            })    
+
+            fs.copyFile('db/templates/room.js', path.join(userStorageDir, 'room.js'), (err) => {
+                if (err) throw err
+            })
+
+            fs.copyFile('db/templates/items.json', path.join(userStorageDir, 'items.json'), (err) => {
+                if (err) throw err
+            })
+
+            await interaction.reply({
+                content: `Welcome aboard, ${username}!` 
+            })
 
         } else {
             interaction.reply({
